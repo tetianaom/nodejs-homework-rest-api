6e007662216c994deef33fbffadde6e813686a9a
Tetiana Omanashvili <tanya.nvt@gmail.com>
2023-08-22 10:29:57 +0300

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,7 @@ const { HttpError } = require("../helpers");
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    next(HttpError(400, `${id} is not valid id`));
+    next(HttpError(400, `id is not defined`));
   }
   next();
 };
